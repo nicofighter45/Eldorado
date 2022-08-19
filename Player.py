0314d33d878bc1f2d8214cfd22c1adbbbde612e6
nicofighter45 <fagot.nicolas.45@gmail.com>
2022-08-19 10:28:44 +0200

    add debt

--- Player.py
+++ Player.py
@@ -5,12 +5,14 @@ def getCleanMoney(money: int) -> str:
 
 class Player:
 
-    def __init__(self, name: str, money: int, gold: int, action_nb: int, actions):
+    def __init__(self, name: str, money: int, gold: int, action_nb: int, debt: int, base_start: int, actions):
         self.name = name
         self.money = money
         self.gold = gold
         self.actionNumber = action_nb
         self.actions = actions
+        self.debt = debt
+        self.base_start = base_start
 
     def buy(self, type: str, value: int) -> str:
         action = self.getAction(type)
@@ -41,17 +43,17 @@ class Player:
     def addMoney(self, value: int) -> str:
         if value <= 0:
             return "You can't add a negative amount of money"
-        self.money += 100 * int(value)
+        self.money += 100 * int(value/100)
         return f"{self.name} has now {getCleanMoney(self.money)}"
 
     def removeMoney(self, value: int) -> str:
         if self.money - value == 0:
             return "You don't have enough money"
-        self.money -= 100 * int(value)
+        self.money -= 100 * int(value/100)
         return f"{self.name} has now {getCleanMoney(self.money)}"
 
     def control(self) -> str:
-        total = int(self.money - 100 * int(0.3 * (self.money + 1000 * (self.actionNumber + self.gold))))
+        total = int(self.money - 100 * int(0.3 * (self.money + 1000 * (self.actionNumber + self.gold))/100))
         if total < 0:
             self.money = 0
             return f"{self.name} still need to pay {getCleanMoney(-total)}"
@@ -89,5 +91,36 @@ class Player:
         return f"{self.name} now has {getCleanMoney(self.money)}"
 
     def start(self) -> str:
-        self.money += 200 * self.gold + 100 * self.actionNumber
+        self.money += self.base_start + 200 * self.gold + 100 * self.actionNumber
+        if self.debt * 500 > self.money:
+            value = getCleanMoney(self.debt*500 - self.money)
+            self.money = 0
+            return f"{self.name} still need to pay {value}"
+        self.money -= self.debt * 500
         return f"{self.name} now has {getCleanMoney(self.money)}"
+
+    def setstart(self, value: int) -> str:
+        if value <= 0:
+            return "Base start must be positive"
+        self.base_start = 100 * int(value/100)
+        return f"{self.name} base start is now {getCleanMoney(self.base_start)}"
+
+    def addDebt(self, value: int) -> str:
+        value = int(value/10000)
+        if value <= 0:
+            return "Debt must be positive"
+        self.debt += value
+        return f"{self.name} now has {getCleanMoney(self.debt*10000)} of debt and {getCleanMoney(self.money)}"
+
+    def removeDebt(self, value: int):
+        if value <= 0:
+            return "You can't remove a negative value"
+        elif self.money < value:
+            return "You don't have enough money"
+        elif self.debt * 10000 < value:
+            self.money -= self.debt * 10000
+            self.debt = 0
+        else:
+            self.money -= 10000 * int(value/10000)
+            self.debt -= int(value/10000)
+        return f"{self.name} now has {getCleanMoney(self.debt * 10000)} of debt and {getCleanMoney(self.money)}"
--- main.py
+++ main.py
@@ -31,25 +31,24 @@ def sellGold(player: Player, value: int) -> str:
 
 
 actions: [Action] = [Action('a', "Avion"), Action('d', "Diamant"), Action('u', "Uranium"), Action('m', "Missile"),
-                     Action('s', "Secret"), Action('p', "Pétrolier")]
+                     Action('s', "Secret"), Action('p', "Petrolier")]
 players: [Player] = []
 
 read = input("To you want to import data ? (yes/no)\n")
 
 
 if read == "yes":
-    with open('game.txt', 'r') as file:
+    with open(input("Name of the file :\n") + '.txt', 'r') as file:
         lines = file.readlines()
         i = 0
         while i < len(lines):
             line = lines[i].split(" ")
-            print(line)
             if i < 6:
-                if line[0] == actions[i].char:
+                if line[0] == actions[i].name:
                     actions[i].value = int(line[1])
                     actions[i].to_buy = int(line[2])
             else:
-                player = Player(line[0], int(line[1]), int(line[2]), int(line[3]), actions)
+                player = Player(line[0], int(line[1]), int(line[2]), int(line[3]), int(line[5]), int(line[6]), actions)
                 gold_number -= int(line[2])
                 a = 1
                 while a <= int(line[4]):
@@ -57,7 +56,7 @@ if read == "yes":
                     player.getAction(action_line[0]).possessors[player] = int(action_line[1])
                     a += 1
                 players.append(player)
-                i += a - 1
+                i += a
             i += 1
 
 else:
@@ -66,7 +65,7 @@ else:
         "\n\n    Players :\n\n  --  Example  --\nnicolas,a;antoine,u\n  ----------------\n\n").split(';')
     for player in init_players:
         data = player.split(",")
-        player = Player(data[0], initial_money, 0, 1, actions)
+        player = Player(data[0], initial_money, 0, 1, 0, 0, actions)
         player.getAction(data[1]).add(player)
         players.append(player)
 
@@ -75,19 +74,24 @@ def printPlayers():
     global players
     print("\n\n\nPlayers :")
     for player in players:
-        print(
-            f"  {player.name}:"
-            f"\n    Money: {getCleanMoney(player.money)}"
-            f"\n    Gold: {player.gold}"
-            f"\n    Action:"
-        )
-        totalMoney = player.money + player.gold * 1000
+        print(f"  {player.name}:\n    Money: {getCleanMoney(player.money)}")
+        if player.debt > 0:
+            print(f"    Debt: {getCleanMoney(10000 * player.debt)}")
+        if player.gold > 0:
+            print(f"    Gold: {player.gold}")
+        if player.base_start > 0:
+            print(f"    Base Start: {getCleanMoney(player.base_start)}")
+        has_action = False
+        totalMoney = player.money + player.gold * 1000 - player.debt * 10000
         for action in actions:
             nb = action.possessors.get(player)
             if not nb is None and nb > 0:
+                if not has_action:
+                    print("    Action:")
+                    has_action = True
                 print(f"      {action.name} : {nb} ({getCleanMoney(action.value)})")
                 totalMoney += nb * action.value
-        print(f"    Total: {getCleanMoney(totalMoney)}")
+        print(f"    Capital: {getCleanMoney(totalMoney)}")
 
 
 def printActions():
@@ -117,18 +121,23 @@ while True:
                   "\n  action                             : get all actions informations"
                   "\n  player                             : get all players informations"
                   "\n  help|?                             : this page"
+                  "\n  export <name>                      : export the game on a txt file"
                   "\n  crises <action>                    : economic crises"
                   "\n  success <action> <amount>          : success for an action and for a given amount"
                   "\n  <player> buyd                      : buy diamond with gold"
-                  "\n  <player> buy <action> '<value>'    : buy an action and set it's value "
+                  "\n  <player> buy <action> '<value>'    : buy an action and set it's value"
                   "\n  <player> sell <action> '<amount>'  : sell a number of action (default one)"
                   "\n  <player> buyg <amount>             : buy gold"
                   "\n  <player> sellg <amount>            : sell gold"
                   "\n  <player> add <amount>              : add money"
                   "\n  <player> rm <amount>               : remove money"
+                  "\n  <player> debt <amount>             : add debt"
+                  "\n  <player> debtrm '<amount>'         : remove debt"
+                  "\n  <player> rm <amount>               : remove money"
                   "\n  <player> control                   : control"
                   "\n  <player> bank                      : get your money from the bank"
                   "\n  <player> start                     : get your money from the start case"
+                  "\n  <player> setstart <amount>         : set bonus start of a player"
                   "\b  calc <calcul>                      : calcul anything"
                   )
         else:
@@ -139,9 +148,10 @@ while True:
                 eco = False
                 for action in actions:
                     if action.char == command[1]:
-                        eco = True
                         for player in action.possessors.keys():
-                            print(player.crises())
+                            if action.possessors.get(player) > 0:
+                                eco = True
+                                print(player.crises())
                 if not eco:
                     print("No one was touch by the crises")
             elif command[0] == "success":
@@ -158,6 +168,20 @@ while True:
                 print(eval(command[1]))
             elif command[1] == "control":
                 print(player.control())
+            elif command[0] == "export":
+                with open(command[1] + ".txt", "w") as file:
+                    for action in actions:
+                        file.write(f"{action.name} {action.value} {action.to_buy}\n")
+                    for player in players:
+                        action_type_nb = 0
+                        action_string = ""
+                        for action in actions:
+                            nb = action.possessors.get(player)
+                            if nb is not None and nb > 0:
+                                action_string += f"{action.char} {nb}\n"
+                                action_type_nb += 1
+                        file.write(f"{player.name} {player.money} {player.gold} {player.actionNumber} {action_type_nb} {player.debt} {player.base_start}\n")
+                        file.write(action_string)
             elif command[1] == "buyd":
                 if actions[1].to_buy < 3:
                     print("There isn't enough diamond")
@@ -165,6 +189,7 @@ while True:
                     print("You don't have enough gold")
                 else:
                     player.gold -= 10
+                    gold_number += 10
                     actions[1].add(player)
                     actions[1].add(player)
                     actions[1].add(player)
@@ -174,6 +199,8 @@ while True:
                 print(player.bank())
             elif command[1] == "start":
                 print(player.start())
+            elif command[1] == "setstart":
+                print(player.setstart(int(command[2])))
             elif command[1] == "buyg":
                 print(buyGold(player, int(command[2])))
             elif command[1] == "sellg":
@@ -188,6 +215,12 @@ while True:
                     print("Error : You don't have enough money")
                 else:
                     print(player.removeMoney(int(command[2])))
+            elif command[1] == "debtrm" and len(command) == 2:
+                print(player.removeDebt(player.debt*1000))
+            elif command[1] == "debtrm":
+                print(player.removeDebt(int(command[2])))
+            elif command[1] == "debt":
+                print(player.addDebt(int(command[2])))
             elif command[1] == "buy" and len(command) == 3:
                 print(player.buy(command[2], player.getAction(command[2]).value))
             elif command[1] == "sell" and len(command) == 3:
